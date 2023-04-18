65a56637fddb8af00d33146d6f7f523fefa21e4b
olvera93 <olvera9302@outlook.com>
2023-04-18 15:52:41 -0600

    [ADD] Skeleton

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 5).theme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Yes No App'),
+        ),
+        body: Center(
+          child: FilledButton(
+            onPressed: () {},
+            child: const Text('Click me'),
+          ),
+        ),
+      ),
+    );
+  }
+}
